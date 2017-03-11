5c76ee873d0614e5c2239b40b6771038ecbe8dcc
mvarxer <mvarxer@163.com>
2017-03-11 23:18:42 +0800

    Initial Commit

---
+++ Crazy2048/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Crazy2048
+//
+//  Created by mvarxer on 17/3/11.
+//  Copyright © 2017年 mvarxer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
